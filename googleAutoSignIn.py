4ed13e2b106088c31678a4547ceb3ff0b124355b
Dylan Pham <32346117+pham-d@users.noreply.github.com>
2018-12-26 18:58:08 -0800

    Create googleAutoSignIn.py
    automated software to sign in to google account

---
+++ googleAutoSignIn.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome('C:\\Users\\Username\\Downloads\\chromedriver.exe') # path to chromedriver.exe
+driver.get('https://google.com')
+signInButton = driver.find_element_by_id('gb_70')
+signInButton.click()
+signIn = driver.find_element_by_id('identifierId')
+signIn.click()
+signIn.send_keys('name@gmail.com') # your email address
+signIn.send_keys(Keys.ENTER)
+time.sleep(1)
+password = driver.find_element_by_name('password')
+password.send_keys('password')  # your password
+password.send_keys(Keys.ENTER)
+
+# download chromedriver.exe from https://sites.google.com/a/chromium.org/chromedriver/downloads
